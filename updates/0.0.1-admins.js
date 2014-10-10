19012708c22fe649f83f6b78ed52fa63d14f7df1
Berkeley Martinez <Berkeley@robotie.com>
2014-10-10 16:38:45 -0700

    Updated script styling TODO:
    * Convert handlebars templates to jade.
    * Move theme components into correct folders
    * refactor gulpfile for new folder structure
    * test run

--- updates/0.0.1-admins.js
+++ updates/0.0.1-admins.js
@@ -5,9 +5,13 @@
  */
 
 exports.create = {
-	User: [
-		{ 'name.first': 'Admin', 'name.last': 'User', email: 'info@r3dm.com', password: 'thebigredmiscoming', isAdmin: true }
-	]
+  User: [{
+    'name.first': 'Admin',
+    'name.last': 'User',
+    email: 'info@r3dm.com',
+    password: 'thebigredmiscoming',
+    isAdmin: true
+  }]
 };
 
 /**
@@ -17,31 +21,35 @@ exports.create = {
  */
 /*
 var keystone = require('keystone'),
-	async = require('async'),
-	User = keystone.list('User');
+  async = require('async'),
+  User = keystone.list('User');
 
-var admins = [
-	{ email: 'user@keystonejs.com', password: 'admin', name: { first: 'Admin', last: 'User' } }
-];
+var admins = [{
+  email: 'user@keystonejs.com',
+  password: 'admin',
+  name: {
+    first: 'Admin',
+    last: 'User'
+  }
+}];
 
 function createAdmin(admin, done) {
-	
-	var newAdmin = new User.model(admin);
-	
-	newAdmin.isAdmin = true;
-	newAdmin.save(function(err) {
-		if (err) {
-			console.error("Error adding admin " + admin.email + " to the database:");
-			console.error(err);
-		} else {
-			console.log("Added admin " + admin.email + " to the database.");
-		}
-		done(err);
-	});
-	
+
+  var newAdmin = new User.model(admin);
+
+  newAdmin.isAdmin = true;
+  newAdmin.save(function(err) {
+    if (err) {
+      console.error("Error adding admin " + admin.email + " to the database:");
+      console.error(err);
+    } else {
+      console.log("Added admin " + admin.email + " to the database.");
+    }
+    done(err);
+  });
 }
 
 exports = module.exports = function(done) {
-	async.forEach(admins, createAdmin, done);
+  async.forEach(admins, createAdmin, done);
 };
 */
