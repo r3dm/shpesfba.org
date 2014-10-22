d63473c54792ae0057cd088098137cdbe2021068
Harry Moreno <morenoh149@gmail.com>
2014-10-22 15:59:37 -0700

    abandoned helper method with bind attempt due to having to then add promise support to call the 'next()' method. Formatted Event fields to camelcase where possible

--- models/Event.js
+++ models/Event.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var keystone = require('keystone'),
   Types = keystone.Field.Types,
   https = require('https');
@@ -5,70 +7,63 @@ var keystone = require('keystone'),
 var Event = new keystone.List('Event');
 
 Event.add({
-  FB_event_id:    { type: Types.Number, required: true, initial: true, unique: true },
+  FBEventId:    { type: Types.Number, required: true, initial: true, unique: true },
   name:           { type: Types.Text },
-  FB_event_name:  { type: Types.Text },
+  FBEventName:  { type: Types.Text },
   description:    { type: Types.Textarea },
-  start_time:     { type: Types.Datetime },
-  end_time:       { type: Types.Datetime },
-  is_date_only:   { type: Types.Boolean },
+  startTime:     { type: Types.Datetime },
+  endTime:       { type: Types.Datetime },
+  isDateOnly:   { type: Types.Boolean },
   location:       { type: Types.Text },
-  owner_name:     { type: Types.Text },
-  updated_time:   { type: Types.Datetime },
-  venue_name:     { type: Types.Text }, // typically given if lat/long data isn't
-  venue_city :    { type: Types.Text },
-  venue_lat :     { type: Types.Number },
-  venue_long :    { type: Types.Number },
-  venue_state :   { type: Types.Text },
-  venue_street :  { type: Types.Text },
-  venue_zip :     { type: Types.Number },
-  cover_photo:    { type: Types.S3File }
+  ownerName:     { type: Types.Text },
+  updatedTime:   { type: Types.Datetime },
+  venueName:     { type: Types.Text }, // typically given if lat/long data isn't
+  venueCity :    { type: Types.Text },
+  venueLat :     { type: Types.Number },
+  venueLong :    { type: Types.Number },
+  venueState :   { type: Types.Text },
+  venueStreet :  { type: Types.Text },
+  venueZip :     { type: Types.Number },
+  coverPhoto:    { type: Types.S3File }
 });
 
 Event.schema.pre('save', function(next) {
   var myEvent = this;
   https.get(
-    "https://graph.facebook.com/v2.1/" + this.FB_event_id +
-    "?access_token=1540860829462325|w119GKY-gtms6pHnBUIMhAUDun4",
+    'https://graph.facebook.com/v2.1/' + this.FBEventId + '?access_token=1540860829462325|w119GKY-gtms6pHnBUIMhAUDun4',
     function(res) {
       var body = '';
-      res.on('data', function(d) {
-        body += d;
-      });
+      res.on('data', function(d) { body += d; });
       res.on('end', function() {
         body = JSON.parse(body);
         if (body.type === 'GraphMethodException') {
-          // flash to user the error
+          // TODO prevent completion of event save
+          console.log('graphMethodException :(');
         } else {
-          myEvent.FB_event_name = body.name;
+          myEvent.FBEventName = body.name;
           myEvent.description = body.description;
-          myEvent.start_time = body.start_time;
-          myEvent.end_time = body.end_time;
+          myEvent.startTime = body.start_time;
+          myEvent.endTime = body.end_time;
           myEvent.is_data_only = body.is_data_only;
           myEvent.location = body.location;
-          myEvent.owner_name = body.owner.name;
-          myEvent.updated_time = body.updated_time;
+          myEvent.ownerName = body.owner.name;
+          myEvent.updatedTime = body.updated_time;
           if (body.venue.name) {
-            myEvent.venue_name = body.venue.name;
-            myEvent.venue_city = null;
-            myEvent.venue_lat = null;
-            myEvent.venue_long = null;
-            myEvent.venue_state = null;
-            myEvent.venue_street = null;
-            myEvent.venue_zip = null;
+            myEvent.venueName = body.venue.name;
+            myEvent.venueCity = myEvent.venueLat = myEvent.venueLong = null;
+            myEvent.venueStreet = myEvent.venueZip = myEvent.venueState = null;
           } else {
-            myEvent.venue_name = null;
-            myEvent.venue_city = body.venue.city;
-            myEvent.venue_lat = body.venue.latitude;
-            myEvent.venue_long = body.venue.longitude;
-            myEvent.venue_state = body.venue.state;
-            myEvent.venue_street = body.venue.street;
-            myEvent.venue_zip = body.venue.zip;
+            myEvent.venueName = null;
+            myEvent.venueCity = body.venue.city;
+            myEvent.venueLat = body.venue.latitude;
+            myEvent.venueLong = body.venue.longitude;
+            myEvent.venueState = body.venue.state;
+            myEvent.venueStreet = body.venue.street;
+            myEvent.venueZip = body.venue.zip;
           }
         }
         next();
       });
-
     }
   ).on('error', function(e) {
     console.log(e);
