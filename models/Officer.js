63a73a58aea1422d64bb20a899904b9e5a89c706
Harry Moreno <morenoh149@gmail.com>
2014-11-07 13:37:30 -0800

    added priority field to officers to easily change the order they get displayed on the contact page o/

--- models/Officer.js
+++ models/Officer.js
@@ -3,15 +3,16 @@
 var keystone = require('keystone'),
   Types = keystone.Field.Types;
 
-var Officer = new keystone.List('Officer');
-Officer.defaultColumns = 'name';
+var Officer = new keystone.List('Officer', { defaultSort: 'priority' });
+Officer.defaultColumns = 'name, priority';
 
 Officer.add({
-  name:   { type: Types.Name, required: true, initial: true },
-  role:   { type: Types.Text },
-  bio:    { type: Types.Textarea },
-  photo:  { type: Types.CloudinaryImage },
-  email:  { type: Types.Email }
+  name:     { type: Types.Name, required: true, initial: true },
+  role:     { type: Types.Text },
+  bio:      { type: Types.Textarea },
+  photo:    { type: Types.CloudinaryImage },
+  email:    { type: Types.Email },
+  priority: { type: Types.Number, sortable: true }
 });
 
 Officer.register();
