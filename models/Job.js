478f7f809fc601e4c667db7c10a59d45c1329e5c
Harry Moreno <morenoh149@gmail.com>
2014-11-11 21:03:27 -0800

    properly styling job listings page

--- models/Job.js
+++ models/Job.js
@@ -7,11 +7,16 @@ var Job = new keystone.List('Job');
 Job.defaultColumns = 'title, expirationDate';
 
 Job.add({
-  title:          { type: Types.Text, required: true, initial: true },
-  description:    { type: Types.Textarea },
-  location:       { type: Types.Location },
-  expirationDate: { type: Types.Date },
-  email:          { type: Types.Email }
+  title:              { type: Types.Text, required: true, initial: true },
+  description:        { type: Types.Textarea },
+  requirements:       { type: Types.Textarea },
+  companyName:        { type: Types.Text },
+  companyBlurb:       { type: Types.Textarea },
+  companyUrl:         { type: Types.Url },
+  location:           { type: Types.Location },
+  expirationDate:     { type: Types.Date },
+  relocationOffered:  { type: Types.Boolean },
+  email:              { type: Types.Email }
 });
 
 Job.register();
