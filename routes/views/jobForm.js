68c872e979cc5e933c5f131c00b6d1f546f7b9f5
Harry Moreno <morenoh149@gmail.com>
2014-11-14 17:11:57 -0800

    Users can now submit job listings to the site.

--- routes/views/jobForm.js
+++ routes/views/jobForm.js
@@ -6,13 +6,11 @@ exports = module.exports = function(req, res) {
       locals = res.locals;
 
   locals.section = 'jobs';
-  // locals.jobTypes = Job.fields.jobType.ops;
   locals.formData = req.body || {};
   locals.validationErrors = {};
   locals.jobSubmitted = false;
 
   view.on('post', { action: 'new' }, function(next) {
-    console.log(locals.formData);
     var newJob = new Job.model({
       title: locals.formData.title,
       description: locals.formData.description,
@@ -20,26 +18,26 @@ exports = module.exports = function(req, res) {
       companyName: locals.formData.companyName,
       companyBlurb: locals.formData.companyBlurb,
       companyUrl: locals.formData.companyUrl,
-      location: locals.formData.location,
+      location: { suburb: locals.formData.city,
+                  state: locals.formData.state},
       expirationDate: locals.formData.expirationDate,
       relocationOffered: locals.formData.relocationOffered,
       email: locals.formData.email
-    });
-    var updater = newJob.getUpdateHandler(req);
+     }), updater = newJob.getUpdateHandler(req);
 
     updater.process(req.body, {
       flashErrors: true
-    }, function(err) {
+     }, function(err) {
       if (err) {
-        locals.validationErrors = err.errors;
+       locals.validationErrors = err.errors;
       } else {
-        req.flash('success', 'Your job has been submitted. Allow 30 days for approval');
+       req.flash('success', 'Your job has been submitted. Allow 30 days for approval.');
 
-        return res.redirect('/jobs');
+       return res.redirect('/jobs');
       }
       next();
-    });
-  });
+     });
+   });
 
   view.render('jobForm', { section: 'jobs' });
-};
+ };
