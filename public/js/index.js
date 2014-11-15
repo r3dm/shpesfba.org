2a8c4b9f173df6506cb087ee248b12870bb9c622
Harry Moreno <morenoh149@gmail.com>
2014-11-14 22:05:49 -0800

    properly displaying pickadate.js in job listing form

--- public/js/index.js
+++ public/js/index.js
@@ -22,32 +22,5 @@ $(document).ready(function() {
     ]
   });
 
-  // only public events can be queried this way
-  var events = [
-    568281156631609,
-    270385519824039,
-    293243587536772,
-    273793429483861,
-    1529142387306200,
-    713616645384315,
-    1530629657165894
-  ];
-
-  // events.forEach(function(element, index, array) {
-  //   $.get(
-  //     "https://graph.facebook.com/v2.1/" + element +
-  //     "?access_token=1540860829462325|w119GKY-gtms6pHnBUIMhAUDun4",
-  //     {},
-  //     function(data) {
-  //       console.log(data);
-  //       a = data;
-  //     }
-  //   );
-  // });
-  //
-  // FB.login(function(){}, {scope: 'user_groups'});
-  //
-  // $('.menu_hamburger-container').menu({
-  // });
-
-});
+  $('.datepicker').pickadate();
+ });
