711674c30c97518c8cc8a69f772afe3b838dbd5e
Harry Moreno <morenoh149@gmail.com>
2014-12-10 16:28:27 -0800

    fixed admin event updating bug. Added protection to templates for when the site has no events

--- models/Event.js
+++ models/Event.js
@@ -27,57 +27,68 @@ Event.add({
   venueStreet :  { type: Types.Text },
   venueZip :     { type: Types.Number, format: false },
   coverPhoto:    { type: Types.S3File },
+  fetchedFromFB: { type: Types.Boolean },
   error:         { type: Types.Boolean }
 });
 
 Event.schema.pre('save', function(next) {
   var myEvent = this;
-  https.get(
-    'https://graph.facebook.com/v2.1/' + this.FBEventId +
-    '?access_token=' + process.env.FB_ACCESS_TOKEN + '|' + process.env.FB_SECRET,
-    function(res) {
-      var body = '';
-      res.on('data', function(d) { body += d; });
-      res.on('end', function() {
-        body = JSON.parse(body);
-        if (body.error) {
-          // TODO prevent completion of event save
-          console.log('graphMethodException :(', body.error);
-          myEvent.FBEventName = 'graphMethodException';
-          myEvent.error = true;
-          myEvent.description = 'We\'ve encountered an error with the provided id.' +
-            'Double check that the event is public';
-        } else {
-          myEvent.FBEventName = body.name;
-          myEvent.description = body.description;
-          myEvent.startTime = body.start_time;
-          myEvent.endTime = body.end_time;
-          myEvent.is_data_only = body.is_data_only;
-          myEvent.location = body.location;
-          myEvent.updatedTime = body.updated_time;
-          myEvent.ownerName = body.owner.name;
-          if (body.venue) {
-            if (body.venue.name) {
-              myEvent.venueName = body.venue.name;
-              myEvent.venueCity = myEvent.venueLat = myEvent.venueLong = null;
-              myEvent.venueStreet = myEvent.venueZip = myEvent.venueState = null;
-            } else {
-              myEvent.venueName = null;
-              myEvent.venueCity = body.venue.city;
-              myEvent.venueLat = body.venue.latitude;
-              myEvent.venueLong = body.venue.longitude;
-              myEvent.venueState = body.venue.state;
-              myEvent.venueStreet = body.venue.street;
-              myEvent.venueZip = body.venue.zip;
+  if (myEvent.fetchedFromFB) {
+    // let it save as normal. Admin must be updating the info
+    next();
+  } else {
+    // this is the first time we save the model. Lets fetch data from Facebook
+    var apiCall = 'https://graph.facebook.com/v2.2/' + this.FBEventId +
+      '?access_token=' + process.env.FB_ACCESS_TOKEN + '|' + process.env.FB_SECRET;
+    console.log('attempting api call:', apiCall);
+    console.log('this object', this);
+    https.get(apiCall,
+      function(res) {
+        var body = '';
+        res.on('data', function(d) { body += d; });
+        res.on('end', function() {
+          body = JSON.parse(body);
+          if (body.error) {
+            // TODO prevent completion of event save
+            console.log('graphMethodException :(', body.error);
+            myEvent.FBEventName = 'graphMethodException';
+            myEvent.error = true;
+            myEvent.fetchedFromFB = false;
+            myEvent.description = 'We\'ve encountered an error with the provided id.' +
+              'Double check that the event is public';
+          } else {
+            myEvent.FBEventName = body.name;
+            myEvent.description = body.description;
+            myEvent.startTime = body.start_time;
+            myEvent.endTime = body.end_time;
+            myEvent.is_data_only = body.is_data_only;
+            myEvent.location = body.location;
+            myEvent.updatedTime = body.updated_time;
+            myEvent.ownerName = body.owner.name;
+            myEvent.fetchedFromFB = true;
+            if (body.venue) {
+              if (body.venue.name) {
+                myEvent.venueName = body.venue.name;
+                myEvent.venueCity = myEvent.venueLat = myEvent.venueLong = null;
+                myEvent.venueStreet = myEvent.venueZip = myEvent.venueState = null;
+              } else {
+                myEvent.venueName = null;
+                myEvent.venueCity = body.venue.city;
+                myEvent.venueLat = body.venue.latitude;
+                myEvent.venueLong = body.venue.longitude;
+                myEvent.venueState = body.venue.state;
+                myEvent.venueStreet = body.venue.street;
+                myEvent.venueZip = body.venue.zip;
+              }
             }
           }
-        }
-        next();
-      });
-    }
-  ).on('error', function(e) {
-    console.log(e);
-  });
+          next();
+        });
+      }
+    ).on('error', function(e) {
+      console.log(e);
+    });
+  }
 });
 
 Event.register();
